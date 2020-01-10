849a90ede9870257fd5bccb0c775dc230e54c20b
D191375 <seedevk8s@gmail.com>
2020-01-10 15:36:53 +0900

    컨테이너 컴포넌트 만들기

--- src/Containers/CounterContainer.js
+++ src/Containers/CounterContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Counter from "../components/Counter";
 import {useSelector, useDispatch } from "react-redux";
+import {decrease, increase, setDiff} from "../modules/counter";
 
 function CounterContainer() {
 
@@ -10,6 +11,15 @@ function CounterContainer() {
         number: state.counter.number,
         diff: state.counter.diff
     }));
+
+    // useDispatch 는 리덕스 스토어의 dispatch 를 함수에서 사용 할 수 있게 해주는 Hook 입니다.
+    const dispatch = useDispatch();
+
+    // 각 액션들을 디스패치하는 함수들을 만드세요
+    const onIncrease = () => dispatch(increase());
+    const onDecrease = () => dispatch(decrease());
+    const onSetDiff = () => dispatch(setDiff());
+
 
     return (
         <Counter>
