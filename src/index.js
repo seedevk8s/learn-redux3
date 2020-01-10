bbc45cdd347fbc0948b59c6285d4bc58454515cc
D191375 <seedevk8s@gmail.com>
2020-01-10 15:09:06 +0900

    리덕스 모듈 만들기

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+//import { Provider } from 'react-redux';
+import { createStore } from "redux";
+import rootReducer from './modules';
+
+// 스토어를 만듭니다.
+const store = createStore(rootReducer);
+// 스토어의 상태를 확인해봅시다.
+console.log(store.getState());
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
